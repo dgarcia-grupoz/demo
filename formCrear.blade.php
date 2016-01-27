f2ebe67e6fb038464eb41610959ec74cb3e027e2
dgarcia-grupoz <dgarcia@grupoz.cl>
2016-01-27 16:19:28 -0300

    v0.1.0
    first commit

---
+++ formCrear.blade.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+ 	<label for="nombre">Nombre</label>
+ 	<input type="text" name="nombre" value="" required></input><br><br>
+ 	<label for="descripcion">Descripcion</label><br>
+ 	<textarea style="width:300px" name="descripcion" required></textarea>
+ 	<br><br>
+</div>
+<br>
